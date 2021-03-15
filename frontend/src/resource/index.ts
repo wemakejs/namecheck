e611cfe19a04f7474b474188b329d1730eda0798
Bill Feng <hello@billfeng.io>
2021-03-15 23:22:44 +0900

    Add resource index

---
+++ frontend/src/resource/index.ts
@@ -0,0 +1,11 @@
+import instagram from "./instagram.svg";
+import twitch from "./twitch.svg";
+import twitter from "./twitter.svg";
+import web from "./web.svg";
+
+export const icons: Record<string, string> = {
+  instagram,
+  twitch,
+  twitter,
+  web,
+};
