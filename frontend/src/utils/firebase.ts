f6273971e74f6c10e1de02156fb35706d67c3e49
Bill Feng <hello@billfeng.io>
2021-03-16 00:47:31 +0900

    Add tld to checkName call

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 
-export const checkName = (name: string, platform: string) => {
+export const checkName = (name: string, platform: string, tld: string) => {
   const func = firebase.functions().httpsCallable("checkName");
-  return func({ name, platform });
+  return func({ name, platform, tld });
 };
