316e6c3b01c2d526a83517dad706249cb9bb95cb
Bill Feng <hello@billfeng.io>
2021-03-15 23:46:55 +0900

    Add more icons

--- frontend/src/resource/index.ts
+++ frontend/src/resource/index.ts
@@ -1,11 +1,15 @@
+import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import twitch from "./twitch.svg";
 import twitter from "./twitter.svg";
 import web from "./web.svg";
+import youtube from "./youtube.svg";
 
 export const icons: Record<string, string> = {
+  facebook,
   instagram,
   twitch,
   twitter,
   web,
+  youtube,
 };
