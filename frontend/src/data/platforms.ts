aaf2960263eeb8380957274afd149dd4b47d7226
Bill Feng <hello@billfeng.io>
2021-03-27 16:04:39 +0900

    Add name validators

--- frontend/src/data/platforms.ts
+++ frontend/src/data/platforms.ts
@@ -1,6 +1,7 @@
 interface Platform {
   id: string;
   getURL: (username: string) => string;
+  isValid: (username: string) => boolean;
   selectedByDefault: boolean;
 }
 interface PlatformGroup {
@@ -8,63 +9,78 @@ interface PlatformGroup {
   platforms: Platform[];
 }
 
+const isValidDomain = (domain: string) => {
+  return !!domain.toLowerCase().match(/^[a-z0-9]+(-[a-z0-9]+)*$/);
+};
+
 export const platformGroups: PlatformGroup[] = [
   {
     id: "domainNames",
     platforms: [
       {
         id: "com",
-        getURL: (username) => `http://${username}.com`,
+        getURL: (domain) => `http://${domain}.com`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "net",
         getURL: (username) => `http://${username}.net`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "org",
         getURL: (username) => `http://${username}.org`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "co",
         getURL: (username) => `http://${username}.co`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "cc",
         getURL: (username) => `http://${username}.cc`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "io",
         getURL: (username) => `http://${username}.io`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "me",
         getURL: (username) => `http://${username}.me`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "bio",
         getURL: (username) => `http://${username}.bio`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "app",
         getURL: (username) => `http://${username}.app`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "page",
         getURL: (username) => `http://${username}.page`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
       {
         id: "zone",
         getURL: (username) => `http://${username}.zone`,
+        isValid: isValidDomain,
         selectedByDefault: true,
       },
     ],
@@ -75,16 +91,21 @@ export const platformGroups: PlatformGroup[] = [
       {
         id: "facebook",
         getURL: (username) => `http://facebook.com/${username}`,
+        // TODO: add username validator
+        isValid: (username) => true,
         selectedByDefault: true,
       },
       {
         id: "instagram",
         getURL: (username) => `http://instagram.com/${username}`,
+        // TODO: add username validator
+        isValid: (username) => true,
         selectedByDefault: true,
       },
       {
         id: "twitter",
         getURL: (username) => `http://twitter.com/${username}`,
+        isValid: (username) => !!username.match(/^[A-Za-z0-9_]{1,15}$/),
         selectedByDefault: true,
       },
     ],
@@ -95,11 +116,15 @@ export const platformGroups: PlatformGroup[] = [
       {
         id: "youtube",
         getURL: (username) => `https://www.youtube.com/${username}`,
+        // TODO: add username validator
+        isValid: (username) => true,
         selectedByDefault: true,
       },
       {
         id: "twitch",
         getURL: (username) => `https://www.twitch.tv/${username}`,
+        // TODO: add username validator
+        isValid: (username) => true,
         selectedByDefault: true,
       },
     ],
@@ -110,6 +135,8 @@ export const platformGroups: PlatformGroup[] = [
       {
         id: "github",
         getURL: (username) => `https://www.github.com/${username}`,
+        // TODO: add username validator
+        isValid: (username) => true,
         selectedByDefault: true,
       },
     ],
