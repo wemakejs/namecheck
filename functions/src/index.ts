6a843c1b5fd6a7c1fc55e14023930f706abda201
Bill Feng <hello@billfeng.io>
2021-03-30 04:56:50 +0900

    Add memory allocation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,20 +2,22 @@ import * as functions from "firebase-functions";
 
 import { checkers } from "./checkers";
 
-export const checkName = functions.https.onCall(async (data) => {
-  const { name, platform, tld = "" } = data;
-  const checker = checkers[platform];
+export const checkName = functions
+  .runWith({ memory: "1GB", timeoutSeconds: 60 })
+  .https.onCall(async (data) => {
+    const { name, platform, tld = "" } = data;
+    const checker = checkers[platform];
 
-  if (
-    typeof name !== "string" ||
-    typeof platform !== "string" ||
-    !name ||
-    !platform ||
-    (platform === "web" && !tld) ||
-    !checker
-  ) {
-    return { error: 401 };
-  }
+    if (
+      typeof name !== "string" ||
+      typeof platform !== "string" ||
+      !name ||
+      !platform ||
+      (platform === "web" && !tld) ||
+      !checker
+    ) {
+      return { error: 401 };
+    }
 
-  return await checker(name + tld);
-});
+    return await checker(name + tld);
+  });
